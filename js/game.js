42f5eebfdf695a9c439424b0e894944993681a42
Njaal Tangye <belbert95@me.com>
2014-10-19 03:42:49 +0200

    Loads of stuff

--- js/game.js
+++ js/game.js
@@ -5,16 +5,6 @@ canvas.width = 1280;
 canvas.height = 720;
 document.body.appendChild(canvas);
 
-// Pad image
-var padReady = false;
-var padImage = new Image();
-padImage.onload = function () {
-	padReady = true;
-};
-
-padImage.src = "images/Pad.png";
-
-
 
 // Game objects
 var circle = 
@@ -72,10 +62,11 @@ addEventListener("mousemove", function (e)
 		angle = Math.PI / 2 + Math.acos(dx / distance)
 	}
 
-	pad.x = 50 + canvas.width * 0.5 + circle.radius * dx / distance// - 32
-	pad.y = 50 + canvas.height * 0.5 - circle.radius * dy / distance //- 13.5
+	pad.x = circle.x
+	pad.y = circle.y
 
 	pad.rotation = angle
+	pad.visualRotation = angle - Math.PI/2
 
 }, false);
 
@@ -84,17 +75,22 @@ var spawnCount = 0;
 // SPAWN BALLS
 var spawnDistance = 200 // Should be increased
 var ballArray = []
+var wasteArray = []
 var ballRadius = 7
 var turnedArray = []
 
 var laserArray = []
 function doMouseDown(event)
 {
-	var laser = new Laser();
-	laser.spawn(6);
+	if (center.redCounter < 200)
+	{var laser = new Laser();
+	laser.spawn(6);}
+	
 }
 
 var spawnLimit = 0.005
+
+var aoeArray = [] 
 
 // Update game objects
 var update = function (modifier) 
@@ -108,7 +104,10 @@ var update = function (modifier)
 
 	}
 
-	
+	aoeArray.forEach(function(blast)
+	{
+		//blast.update();
+	});
 };
 
 var circleX = canvas.width / 2
@@ -118,20 +117,30 @@ var circleY = canvas.height / 2
 var render = function (deltaTime) 
 {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = '#36A8E0';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	aoeArray.forEach(function(blast)
+	{
+		blast.draw();
+	});
+
+	centerColor = "rgb(" +String(center.redCounter)+", 0, 0)";
+	center.redCounter = Math.max(center.redCounter-1, 0)
 	center.draw()
 
-	if (padReady) 
-	{
-		drawRotatedImage(padImage, pad.x, pad.y, pad.rotation);
-	}
+	pad.draw()
    
     turnedArray.forEach(function(turned)
     {
     	turned.moveIntoOrbit()
         turned.circleCounter += turned.circleSpeed
-		
-		turned.x += -Math.sin(turned.circleCounter + turned.crashAngle) + turned.errorSpeedX * 0.01
-		turned.y += Math.cos(turned.circleCounter + turned.crashAngle) + turned.errorSpeedY * 0.01
+
+		turned.vector[0] = -Math.sin(turned.circleCounter + turned.crashAngle) + turned.errorSpeedX * 0.01
+		turned.vector[1] = Math.cos(turned.circleCounter + turned.crashAngle) + turned.errorSpeedY * 0.01
+		turned.x += turned.vector[0]
+		turned.y += turned.vector[1]
 
 		if (turned.circleCounter >= 2*Math.PI)
 		{
@@ -140,12 +149,28 @@ var render = function (deltaTime)
 
 		for (var e = 0; e < ballArray.length; e++)
 		{
-			ball2 = ballArray[e]
-
-			if (turned.testCollision(ball2) == true)
+			var ball2 = ballArray[e]
+			//console.trace(ball2)
+			if (ball2 != turned)
 			{
-				turned.handleCollision(ball2)
+				if (turned.testCollision(ball2) == true)
+				{
+					turned.handleCollision(ball2, true)
+				}
 			}
+		}
+
+		for (var f = 0; f < turnedArray.length; f++)
+		{
+			var ballf = turnedArray[f]
+			//console.trace(ballf)
+			if (ballf != turned)
+			{
+				if (turned.testCollision(ballf) == true)
+				{
+					turned.handleCollision(ballf, false)
+				}
+			}	
 		}
 
 		turned.draw()
@@ -154,7 +179,6 @@ var render = function (deltaTime)
      
     ballArray.forEach(function(ball)
     {
-         
         ball.flightCounter += 1;
 								
 		ball.x = ball.startX + ball.vector[0] * ball.flightCounter;
@@ -162,7 +186,7 @@ var render = function (deltaTime)
          
         if (ball.flightCounter == Math.round(ball.crashTime) && ball.expectedToCrash == true)
         {
-        	if (Math.abs(ball.crashAngle-pad.rotation) < Math.PI/3)
+        	if (Math.abs(ball.crashAngle-pad.rotation) < Math.PI/2)
         	{
         		//ball.giveDirection(ball.spawnX, ball.spawnY, false)
         		ball.turn()
@@ -171,25 +195,69 @@ var render = function (deltaTime)
         	else
         	{
 	        	circle.radius -= 5
-	        	pad.x -= 5
-	        	pad.y -= 5
+	        	center.x-=5
+	        	center.y-=5
+	        	ballArray.splice(ballArray.indexOf(ball),1)
+	        	pad.draw()
         	}
         }
        
         ball.draw();
 	});
 
+	/*wasteArray.forEach(function(waste)
+    {
+    	//console.trace(waste.testCollision(circle) == true)
+    	if (waste.testCollision(circle) == true)
+    	{
+    		waste.vector[0] = -waste.vector[0]
+    		waste.vector[1] = -waste.vector[1]
+    	}
+
+    	for (var r = 0; r < ballArray.length; r++)
+		{
+			var ball4 = ballArray[r]
+
+			if (ball4 != waste)
+			{
+				if (waste.testCollision(ball4))
+				{
+					waste.handleCollision(ball4)
+				}
+			}
+			//console.trace(ball4)
+			
+		}
+
+	});*/
+
 	laserArray.forEach(function(laser)
 	{
 		laser.flightCounter += 1;
-		
-		console.trace(laser.startX, laser.vector[0], laser.flightCounter)					
+						
 		laser.x = laser.startX + laser.vector[0] * laser.flightCounter;
 		laser.y = laser.startY - laser.vector[1] * laser.flightCounter;
 
+		for (var y = 0; y < ballArray.length; y++)
+		{
+			var ball3 = ballArray[y]
+			if (laser.testCollision(ball3))
+			{
+				ballArray.splice(y, 1)
+			}
+		}
+		
+
 		laser.drawLaser()
 	});
+
+
+
+	ctx.font="30px Georgia";
+	ctx.fillText(String(Math.floor((Date.now()-startTime)/1000)),100,200)
 };
+
+var startTime = Date.now();
 
 // The main game loop
 var main = function () 
@@ -211,15 +279,48 @@ var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 
-var ballColor = '#00ff00';
-var centerColor = '#32cd32';	
+var ballColor = '#DB2EC7';
+var centerColor = "rgb(82, 92 ,209)"; //(R,G,B)
+
+var pad = new Pad()
 
 // Let's play this game!
 var then = Date.now();
 main();
 
+function Pad()
+{
+	this.draw = function()
+	{
+		ctx.fillStyle = ballColor;
+         
+        /*ctx.beginPath();
+         
+        ctx.arc(pad.x, pad.y, circle.radius, pad.rotation - Math.PI/2, pad.rotation, false);
+        ctx.fill();
+         
+        ctx.closePath();
+
+        ctx.moveTo(pad.x,pad.y);
+		ctx.arc(pad.x,pad.y,circle.radius,pad.rotation - Math.PI/6 - Math.PI/2 ,pad.rotation + Math.PI/6 - Math.PI/2);
+		ctx.lineTo(pad.x,pad.y);
+		ctx.stroke(); // or context.fill()*/
+
+		ctx.beginPath();
+      	ctx.arc(pad.x, pad.y, circle.radius+5, pad.visualRotation - Math.PI/4, pad.visualRotation + Math.PI/4, false);
+      	ctx.lineWidth = 10;
+
+      	// line color
+      	ctx.strokeStyle = 'green';
+      	ctx.stroke();
+	}
+}
+
+
 function Center()
 {
+	this.reloading = false
+	this.redCounter = 0
 	this.draw = function()
 	{
 		ctx.fillStyle = centerColor;
@@ -237,15 +338,14 @@ function Ball()
 {
 	this.spawn = function(speed)
 	{
-		spawnCount++;
-		console.trace(spawnCount)
 		this.expectedToCrash = true
 
 		this.friendly = false
 		this.radius = 7
 		this.color = ballColor
-		this.x = canvas.width / 2 + 400*Math.cos(Math.random() * 2 * Math.PI)
-	 	this.y = canvas.height / 2 + 400*Math.sin(Math.random() * 2 * Math.PI)
+		var rNumber = Math.random()
+		this.x = canvas.width / 2 + 400*Math.cos(rNumber* 2 * Math.PI)
+	 	this.y = canvas.height / 2 + 400*Math.sin(rNumber * 2 * Math.PI)
 	 	this.spawnX = this.x
 	 	this.spawnY = this.y
 	 	this.speed = speed
@@ -256,42 +356,40 @@ function Ball()
 
 	this.turn = function()
 	{
-		console.trace("Turned")
-		console.trace("BallArray pre: ", ballArray)
 		ballArray.splice(ballArray.indexOf(this), 1)
-		console.trace("BallArray post: ", ballArray)
 		turnedArray[turnedArray.length] = this
 
+		this.color = "#0000ff"
+
 		this.crashAngle -= Math.PI / 2
-		console.log("Crashangle: ", this.crashAngle)
 
 		this.circleCounter = 0
 		this.circleSpeed = 1 / (100)
 
 		//this.x = (canvas.width * 0.5 + 50) + 100 * Math.cos((this.crashAngle));
 		//this.y = (canvas.height * 0.5 + 50) + 100 * Math.sin((this.crashAngle));
-
+		this.expectedToCrash = false
 		this.errorSpeedX = 0
 		this.errorSpeedY = 0
 	}
 
 	this.moveIntoOrbit = function()
 	{
-		var refX = canvas.width * 0.5 + 50 + 100*Math.cos(this.circleCounter + this.crashAngle)
-		var refY = canvas.height * 0.5 + 50 + 100*Math.sin(this.circleCounter + this.crashAngle)
+		var refX = canvas.width * 0.5 + 50 + 300*Math.cos(this.circleCounter + this.crashAngle)
+		var refY = canvas.height * 0.5 + 50 + 300*Math.sin(this.circleCounter + this.crashAngle)
 
 		this.errorSpeedX = refX-this.x
 		this.errorSpeedY = refY-this.y
 	}
 
-	this.testCollision = function(ball2)
+	this.testCollision = function(ball1)
 	{
-		var dx = ball2.x - this.x
-		var dy = ball2.y - this.y
+		var dx = ball1.x - this.x
+		var dy = ball1.y - this.y
 
 		var distance = Math.sqrt(dx*dx+dy*dy)
 
-		if (distance < ball2.radius + this.radius)
+		if (distance < ball1.radius + this.radius)
 		{
 			return true
 		}
@@ -301,25 +399,53 @@ function Ball()
 		}
 	}
 
-	this.handleCollision = function(ball2)
+	this.handleCollision = function(ball2, statusChange)
 	{
-		var angle = Math.atan2(ball2.x - this.x, this.y - ball2.y);
+		console.trace("Kræsj")
+		var dx = ball2.x-this.x
+		var dy = this.x-ball2.y
+		var distance = this.radius + ball2.radius
 
-        var targetX = this.x + Math.cos(angle) * (ball2.radius + this.radius);
+		var XaxisAngle = Math.atan2(dy, dx)
+		var YaxisAngle = Math.atan2(dy, dx) - Math.PI/2
 
-        var targetY = this.y + Math.sin(angle) * (ball2.radius + this.radius);
+		var vx = ball2.vector[0]
+		var vy = ball2.vector[1]
 
-        var ax = (targetX - ball2.x) * 0.05;
+		var vBallAngle = Math.atan2(vy, vx)
 
-        var ay = (targetY - ball2.y) * 0.05;
+		var vThisAngle = Math.atan2(this.vector[0], this.vector[1])
 
-        //this.vector[0] -= ax;
+		var ballResult = YaxisAngle + vThisAngle
+		var thisResult = YaxisAngle + vBallAngle
 
-        //this.vector[1] -= ay
+		if (statusChange == true)
+		{
+			if (turnedArray.indexOf(this) > -1)
+			{
 
-        ball2.vector[0] += ax;
-
-        ball2.vector[1] += ay;
+				turnedArray.splice(turnedArray.indexOf(this), 1)
+			}
+			ballArray[ballArray.length] = this
+			this.color = "black"
+			ball2.color = "black"
+		}
+		
+		this.flightCounter = 0
+		this.crashTime = 500	
+		this.startX = this.x
+		this.startY = this.y
+		this.vector[0] = Math.cos(thisResult)
+		this.vector[1] = Math.sin(thisResult)
+		this.expectedToCrash = false
+		
+		ball2.expectedToCrash = false
+		ball2.flightCounter = 0
+		ball2.startX = ball2.x
+		ball2.startY = ball2.y
+		ball2.vector[0] = Math.cos(ballResult)
+		ball2.vector[1] = Math.sin(ballResult)
+		
 	}
 
 	this.giveDirection = function(toX, toY, expectedToCrash)
@@ -344,11 +470,10 @@ function Ball()
 		{
 			var sx = this.x - (canvas.width * 0.5 + 50)
 		   	var sy = canvas.height * 0.5 + 50  - this.y
-		   	var circleHit = circle.radius + 2
+		   	var circleHit = circle.radius + 10
 			
 			// hehehhe:
 			this.crashTime = -(Math.sqrt(-4*(this.vector[0]*this.vector[0]+this.vector[1]*this.vector[1])*(-(this.radius + circleHit)*(this.radius + circleHit)+sx*sx+sy*sy) + (2*sx*this.vector[0]+2*sy*this.vector[1])*(2*sx*this.vector[0]+2*sy*this.vector[1]))+2*sx*this.vector[0]+2*sy*this.vector[1])/(2*this.vector[0]*this.vector[0]+2*this.vector[1]*this.vector[1])
-			console.log("Crashes in: ", this.crashTime)
 
 			this.crashAngle = Math.cos(-dx / Math.sqrt(dx*dx + dy*dy))
 
@@ -380,14 +505,15 @@ function Ball()
 
 addEventListener("mousedown", doMouseDown, false);
 
-
-
 var laserColor = '#ff0000';
 
 function Laser()
 {
 	this.spawn = function(speed)
 	{
+		center.reloading = true
+		center.redCounter = Math.min(center.redCounter + 100, 255)
+		//console.trace(center.redCounter)
 		this.flightCounter = 0
 		this.radius = 7
 		this.speed = speed
@@ -415,6 +541,23 @@ function Laser()
 		this.vector = [-this.speed * dx / this.absvector,- this.speed * dy / this.absvector];
 	}
 
+	this.testCollision = function(ball2)
+	{
+		var dx = ball2.x - this.x
+		var dy = ball2.y - this.y
+
+		var distance = Math.sqrt(dx*dx+dy*dy)
+
+		if (distance < ball2.radius + this.radius)
+		{
+			return true
+		}
+		else
+		{
+			return false
+		}
+	}
+
 	this.drawLaser = function()
 	{
 		
@@ -428,6 +571,65 @@ function Laser()
         ctx.closePath();
 	};
 }
+
+addEventListener("keydown", keyboard, true);
+
+function keyboard(e)
+{
+	if(e.keyCode === 87) //32 = space
+	{
+		var blast = new AoEBlast();
+		blast.spawn();
+	}
+}
+
+//var blastColor = '#D424BF';
+var blastColor = '#ff0000';
+
+var blastRadius = center.radius;
+
+function AoEBlast()
+{
+	this.spawn = function()
+	{
+		//var startBlastTime = Date.now();
+
+		this.color = blastColor;
+		this.radius = 200;
+		this.x = center.x;
+		this.y = center.y;
+
+		var maxRadius = this.radius * 2;
+
+		aoeArray[aoeArray.length] = this	
+	};	
+
+	//var timer = Date.now();
+
+	this.update = function()
+	{
+		//timer -= startBlastTime / 1000;
+
+		while(this.radius < maxRadius)
+		{
+			radius += .5;
+		}
+	};
+
+	this.draw = function()
+	{
+		ctx.fillStyle = blastColor;
+         
+        ctx.beginPath();
+         
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fill();
+         
+        ctx.closePath();
+	};
+	
+}
+
 
 
 
--- website/enemyball.js
+++ website/enemyball.js
@@ -1,3 +1,58 @@
-var ballfactory = {
+var Ballfactory = function() {
 
+	cooldown = 1;
+	currentCooldown = 0;
+
+	this.update = function(deltatime) {
+		this.calculateBps();
+
+		currentCooldown += deltatime;
+		if(currentCooldown >= cooldown) {
+			this.spawn();
+		
+	}
+
+	this.calculateBps = function() {
+		//Increases bps every 2*pi seconds by 1
+		var ballsPerSecond = Math.floor(totaltime / (2 * Math.PI * 1000));
+
+		//the gamejam way to prevent divide by zero
+		cooldown = 1 / (ballsPerSecond + 0.001);
+	}
+
+	this.spawn = function() {
+		var tmpBall = new Ball();
+		tmpBall.angle = Math.random() * Math.PI * 2;
+		tmpBall.speed = 1 + Math.random() * 2;
+		balls.push(tmpBall);
+	}
+}
+
+var Ball = function() {
+	x = 0;
+	y = 0;
+	speed = 0; //positive speed means the ball is approaching
+	distance = 0;
+	isFriendly = false;
+
+	this.update = function(deltatime) {
+		//check collision, if collide -> speed *= -1
+		//check if collision with pad, if collide -> isFriendly = true
+
+		if(isFriendly = false) {
+		//angle = const.
+			//distance -= speed * deltatime
+			
+		} else {
+			//orbit around player (distance = semi-major axis/distance to player = const.)
+			//angle += angularVelocity * deltatime;
+		}
+
+		//polar coords to cartesian
+			//x = distance * cos(angle)
+			//y = distance * sin(angle)
+	};
+	this.draw = function(ctx) {
+		//draw ball at x, y
+	};
 };
